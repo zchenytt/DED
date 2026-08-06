3ecdfd7dc83cf07d19ef22c31cfaf19be5e26f62
zchenytt <zchenytt@gmail.com>
2026-08-06 22:48:48 +0800

    Update Ben.jl

--- case118/BinLink/Ben.jl
+++ case118/BinLink/Ben.jl
@@ -1,105 +1,103 @@
 module Ben
 import ..Models, ..Settings, Gurobi
 
-const W_naive = Sys.CPU_THREADS
-const W_fg = 1
-const W_bg = W_naive - W_fg
+const EVAL_MIPGap = 1e-4
+const EVAL_TimLim = 45.
+const MAIN_MIPGap = 9e-4
+const MAIN_TimLim = 15. # 🟥 if MIP-sub is too hard, tune its hardness down!
+const W_fg, W_bg = Threads.nthreads(:interactive), Threads.nthreads(:default)
 const ϵ = Models.SB_CUT_COT
 
-# 🟥 The final global optimality gap relies on the hardness of the subMIPs that are solved to suboptimality
-# if the subMIPs are coarsely solved, the final global optimality gap can be large (e.g. rgap = 0.24).
-config_sub_mips(sub; MIPGap=9e-4, TimeLimit=12.) = Threads.@threads(for s=eachindex(sub)
-    _en2 = Gurobi.GRBgetenv(sub[s].o)
-    Gurobi.GRBsetdblparam(_en2, "MIPGap", MIPGap)
-    Gurobi.GRBsetdblparam(_en2, "TimeLimit", TimeLimit)
-end)
-
-function MIP_warm(mst, sub)
-    (; S, N, Θ, Σt) = mst
-    @assert S > W_bg
-    Settings.opt_ass_opt(mst); load_che(mst)
-    config_sub_mips(sub)
-    Threads.@threads for s = eachindex(sub)
-        n = sub[s]
-        n.Xl .= mst.Xl # make a local static copy
-        (; o, Bv, Pi, Xl, Xl2, Ci, Cd) = n
-        Bv .= 'C';          Gurobi.GRBsetcharattrarray(o, "VType", 0, length(Bv), Bv)
-        Xl2 .= 0;           Gurobi.GRBsetdblattrarray(o, "Obj", 0, N, Xl2)
-                            Gurobi.GRBsetdblattrarray(o, "LB", 0, N, Xl)
-                            Gurobi.GRBsetdblattrarray(o, "UB", 0, N, Xl)
-        Settings.opt_ass_opt(n, "warm - LP")
-        Gurobi.GRBgetdblattrarray(o, "RC", 0, N, Pi)
-        @. Xl2 = 0 - Pi;    Gurobi.GRBsetdblattrarray(o, "Obj", 0, N, Xl2)
-        Xl2 .= 0;           Gurobi.GRBsetdblattrarray(o, "LB", 0, N, Xl2)
-        Xl2 .= 1;           Gurobi.GRBsetdblattrarray(o, "UB", 0, N, Xl2)
-        Bv .= 'B';          Gurobi.GRBsetcharattrarray(o, "VType", 0, length(Bv), Bv)
-        Settings.opt_ass_time(n, "warm - biased subMIP")
-        Cd[1:N] .= Pi
-        Cd[end]  = Obn = Settings.getmodeldblattr(n, "ObjBound")
+function _6(ch, sub, s, Xl)
+    (; o, N, Bv, Pi, Xl2, Cd) = n = sub[s]
+    Bv .= 'C'; Gurobi.GRBsetcharattrarray(o, "VType", 0, length(Bv), Bv)
+    Xl2 .= 0;  Gurobi.GRBsetdblattrarray(o, "Obj", 0, N, Xl2)
+               Gurobi.GRBsetdblattrarray(o, "LB", 0, N, Xl)
+               Gurobi.GRBsetdblattrarray(o, "UB", 0, N, Xl)
+    Settings.opt_ass_opt(n, "no bias sub-LP")
+    Cd[N+1] = obj = Settings.getmodeldblattr(n, "ObjVal")
+    Gurobi.GRBgetdblattrarray(o, "RC", 0, N, Pi)
+    Cd[1:N] .= Pi
+    Cd[end] = Cn = obj - Pi'Xl
+    put!(ch, s)
+end
+_7(ch, sub, s, Xl) = Threads.@spawn(_6(ch, sub, s, Xl))
+function add_a_single_θ_cut(mst, sub; mstVType)
+    (; o, S, N, Θ, Xl, Σt, Bv, ch, Ci, Cd) = mst
+    mstVType === 'B' || error("not planned")
+    Bv .= mstVType; Gurobi.GRBsetcharattrarray(o, "VType", S, N, Bv)
+    Settings.opt_ass_opt(mst)
+    Gurobi.GRBgetdblattrarray(o, "X", S, N, Xl)
+    @. Xl = round(Xl); xHa = hash(Xl)
+    θμ_che = Settings.getxdblattrelement(mst, S+N, "X")
+    i = 1
+    for _=1:W_bg _7(ch, sub, i, Xl); i+=1 end
+    while i ≤ S
+        take!(ch)
+        _7(ch, sub, i, Xl); i+=1
+    end
+    for _=1:W_bg take!(ch) end
+    ub_LP = sum(n.Cd[N+1] for n=sub)/S
+    ν = ub_LP - θμ_che
+    @ccall(printf("x_int_che = %llx, vio_by_LP = %.4e\n"::Cstring; xHa::UInt64, ν::Cdouble)::Cint)
+    cut_added = ν > ϵ
+    if cut_added
+        Cd .= 0
+        for n=sub Cd .+= n.Cd end
+        Cd ./= S
         Cd[N+1] = -1
+        Gurobi.GRBaddconstr(o, N+1, Ci, Cd, Cchar('<'), -Cd[end], C_NULL)
     end
-    for n=sub Gurobi.GRBaddconstr(mst.o, N+1, n.Ci, n.Cd, Cchar('<'), -n.Cd[end], C_NULL) end
-    Θ .= 1/S; Gurobi.GRBsetdblattrarray(mst.o, "Obj", 0, S, Θ)
-    Settings.opt_ass_opt(mst, "The 1st time optimize with Objective properly set")
-    load_che(mst)
-    Σt.x += Settings.getmodeldblattr(mst, "Runtime")
+    cut_added, xHa
 end
-function get_lb_ub(mst, sub)
-    (; S, N, Θ) = mst
-    mst.Bv .= 'B'; Gurobi.GRBsetcharattrarray(mst.o, "VType", S, N, mst.Bv)
-    Settings.opt_ass_opt(mst); load_che(mst)
-    config_sub_mips(sub; MIPGap=1e-4, TimeLimit=50.)
-    Threads.@threads for s = eachindex(sub)
-        (; o, Bv, Pi, Xl, Xl2, Ci, Cd) = n = sub[s]
-        @. Xl = round(mst.Xl)
-        Bv .= 'C'; Gurobi.GRBsetcharattrarray(o, "VType", 0, N, Bv)
-        Xl2 .= 0;  Gurobi.GRBsetdblattrarray(o, "Obj", 0, N, Xl2)
-                   Gurobi.GRBsetdblattrarray(o, "LB", 0, N, Xl)
-                   Gurobi.GRBsetdblattrarray(o, "UB", 0, N, Xl)
-        Bv .= 'B'; Gurobi.GRBsetcharattrarray(o, "VType", N, length(Bv)-N, Bv)
-        Settings.opt_ass_time(n, "no bias subMIP")
-    end
-    GRBagapErr = ub = 0.
-    for (s,n)=enumerate(sub)
-        ub += ObjVal = Settings.getmodeldblattr(n, "ObjVal")
-        GRBagapErr += Settings.getmodeldblattr(n, "MIPGap")abs(ObjVal)
-        Θ[s] = ObjVal - Θ[s] # update the agap
-    end
-    (agapmin, agapmax), agapsum = extrema(Θ), sum(Θ)
-    rgap, agap = agapsum/ub, agapsum/S
-    ub /= S
-    printstyled(
-        "ub = $ub, rgap = $rgap; agap=(($agapmin,$agapmax), ($agapsum - $GRBagapErr)/$S)",
-        " for other details, check `mst.Θ`.\n"; color = :magenta
-    )
+
+function get_ub(s::Int, Xl::Vector{Float64}, sub) # ub of Q_s(x)
+    (; o, N, Bv, Pi, Xl2, Cd) = n = sub[s]
+    Gurobi.GRBsetdblparam(Gurobi.GRBgetenv(o), "MIPGap", EVAL_MIPGap)
+    Gurobi.GRBsetdblparam(Gurobi.GRBgetenv(o), "TimeLimit", EVAL_TimLim)
+    Bv .= 'C'; Gurobi.GRBsetcharattrarray(o, "VType", 0, N, Bv)
+    Xl2 .= 0;  Gurobi.GRBsetdblattrarray(o, "Obj", 0, N, Xl2)
+               Gurobi.GRBsetdblattrarray(o, "LB", 0, N, Xl)
+               Gurobi.GRBsetdblattrarray(o, "UB", 0, N, Xl)
+    Bv .= 'B'; Gurobi.GRBsetcharattrarray(o, "VType", N, length(Bv)-N, Bv)
+    Settings.opt_ass_time(n, "no bias sub-MIP eval")
+    Cd[end] = ub = Settings.getmodeldblattr(n, "ObjVal")
+    Cd[N+1] = agap = Settings.getmodeldblattr(n, "MIPGap")abs(ub)
 end
+function get_ub(Xl::Vector{Float64}, sub)
+    Threads.@threads for s=eachindex(sub)
+        get_ub(s, Xl, sub)
+    end
+    ub = sum(n.Cd[end] for n=sub)/length(sub)
+    errsum = sum(n.Cd[end-1] for n=sub)
+    erravg = errsum/length(sub)
+    printstyled("ub = $ub, erravg = $erravg, errsum = $errsum"; color = 27)
+    ub
+end
+
 
 function _4(ch, s, Θs, n, Vμ)
     (; o, N, Bv, Pi, Xl, Xl2, Cd) = n
-    Bv .= 'C';          Gurobi.GRBsetcharattrarray(o, "VType", 0, length(Bv), Bv)
-    Xl2 .= 0;           Gurobi.GRBsetdblattrarray(o, "Obj", 0, N, Xl2)
-    Gurobi.GRBsetdblattrarray(o, "LB", 0, N, Xl)
-    Gurobi.GRBsetdblattrarray(o, "UB", 0, N, Xl)
-    Settings.opt_ass_opt(n, "train - subLP")
+    _en2 = Gurobi.GRBgetenv(o)
+    Gurobi.GRBsetdblparam(_en2, "TimeLimit", MAIN_TimLim)
+    Gurobi.GRBsetdblparam(_en2, "MIPGap", MAIN_MIPGap)
+    Bv .= 'C';       Gurobi.GRBsetcharattrarray(o, "VType", 0, length(Bv), Bv)
+    Xl2 .= 0;        Gurobi.GRBsetdblattrarray(o, "Obj", 0, N, Xl2)
+                    Gurobi.GRBsetdblattrarray(o, "LB", 0, N, Xl)
+                    Gurobi.GRBsetdblattrarray(o, "UB", 0, N, Xl)
+    Settings.opt_ass_opt(n, "no bias - subLP")
     Gurobi.GRBgetdblattrarray(o, "RC", 0, N, Pi)
-    @. Xl2 = 0 - Pi;    Gurobi.GRBsetdblattrarray(o, "Obj", 0, N, Xl2)
-    Xl2 .= 0;           Gurobi.GRBsetdblattrarray(o, "LB", 0, N, Xl2)
-    Xl2 .= 1;           Gurobi.GRBsetdblattrarray(o, "UB", 0, N, Xl2)
-    Bv .= 'B';          Gurobi.GRBsetcharattrarray(o, "VType", 0, length(Bv), Bv)
-    Settings.opt_ass_time(n, "train - subMIP")
     Cd[1:N] .= Pi
-    Cd[end]  = Obn = Settings.getmodeldblattr(n, "ObjBound")
-    GRBrgap = Settings.getmodeldblattr(n, "MIPGap")
+    @. Xl2 = 0 - Pi; Gurobi.GRBsetdblattrarray(o, "Obj", 0, N, Xl2)
+    Xl2 .= 0;        Gurobi.GRBsetdblattrarray(o, "LB", 0, N, Xl2)
+    Xl2 .= 1;        Gurobi.GRBsetdblattrarray(o, "UB", 0, N, Xl2)
+    Bv .= 'B';       Gurobi.GRBsetcharattrarray(o, "VType", 0, length(Bv), Bv)
+    Settings.opt_ass_time(n, "biased - subMIP")
+    GRBrgap, Rt = Settings.getmodeldblattr(n, "MIPGap"), Settings.getmodeldblattr(n, "Runtime")
     GRBagap = abs(Settings.getmodeldblattr(n, "ObjVal"))GRBrgap
-    Rt = Settings.getmodeldblattr(n, "Runtime")
-    Cd[N+1]  = vio = Pi'Xl + Obn - Θs
-    vio > Vμ/2 && @ccall(printf("s=%d, GRBgap=(r=%.1e, a=%.1f; t=%.1f), vio=%.4e ◀\n"::Cstring;
-        s::Cint,
-        GRBrgap::Cdouble,
-        GRBagap::Cdouble,
-        Rt::Cdouble,
-        vio::Cdouble
-    )::Cint)
+    Cd[end] = Obn = Settings.getmodeldblattr(n, "ObjBound")
+    Cd[N+1] = ν = Pi'Xl + Obn - Θs # level of separation, at the current x_che
+    ν>0 && @ccall(printf("  GRBgap=(r=%.1e, a=%.1f; t=%.1f), vio=%.4e <s=%d\n"::Cstring;GRBrgap::Cdouble,GRBagap::Cdouble,Rt::Cdouble,ν::Cdouble,s::Cint)::Cint)
     put!(ch, s)
 end
 function _5(ch, s, sub, mst, CanSpawn, Vμ)
@@ -107,53 +105,104 @@ function _5(ch, s, sub, mst, CanSpawn, Vμ)
     n.Xl .= mst.Xl
     CanSpawn[s], _ = false, Threads.@spawn(_4(ch, s, Θs, n, Vμ))
 end
-function main(mst, sub, SeqSeconds, AbsSeconds)
-    (; o, S, N, Θ, Σt, Bv, Vv) = mst
-    VΣ=sum(Vv); Vμ=VΣ/S; α = .5
+function root_train(mst, sub, SeqSeconds, AbsSeconds)
+    (; o, S, N, Θ, Σt, Bv, Vv, CanSpawn, ch, rng, proceed) = mst
+    CanSpawn .= true;       VΣ=sum(Vv); Vμ=VΣ/S; α = .5
     Bv .= 'C'; Gurobi.GRBsetcharattrarray(o, "VType", S, N, Bv)
-    config_sub_mips(sub)
-    ch, CanSpawn = Channel{Int}(), trues(S)
-    Settings.opt_ass_opt(mst); load_che(mst)  
-    i, spnˈd, ȷ = rand(1:S), 0, 0
-    while true # load the hardware
-        spnˈd == W_bg && break
-        _5(ch, i, sub, mst, CanSpawn, Vμ); spnˈd += 1
+    Settings.opt_ass_opt(mst); load_che(mst); lb = Settings.getmodeldblattr(mst, "ObjBound")
+    ȷ, t0 = 0, time_ns(); i = i0 = rand(rng, 1:S)
+    for _=1:W_bg # load the hardware
+        _5(ch, i, sub, mst, CanSpawn, Vμ)
         i = ifelse(i<S, i+1, 1)
     end
-    spnˈold = spnˈd; t0 = time_ns()
-    while true
+    iA = 0
+    while proceed.value
         (t = 1e-9(time_ns() - t0)) > AbsSeconds && break
-        if ȷ > 0 && (isready(ch) === false || spnˈd > spnˈold + W_bg)
-            Settings.opt_ass_opt(mst, "mst_reopt"); load_che(mst)
-            st = Σt.x += Settings.getmodeldblattr(mst, "Runtime")
-            st > SeqSeconds && break
+        if ȷ > 0 && (!isready(ch) || i === i0)
+            Settings.opt_ass_opt(mst); load_che(mst)
+            lb = Settings.getmodeldblattr(mst, "ObjBound")
+            (st = Σt.x += Settings.getmodeldblattr(mst, "Runtime")) > SeqSeconds && break
             nCμ = Settings.getmodelintattr(mst, "NumConstrs")/S
-            @ccall(printf("▶ t=%.1fs, ȷ=%d, nCμ=%.1f, st=%.1fs, Vμ=%.1e\n"::Cstring;
-            t::Cdouble, ȷ::Cint, nCμ::Cdouble, st::Cdouble, Vμ::Cdouble)::Cint)
-            ȷ = 0
-            spnˈold = spnˈd
+            @ccall(printf("M> t=%.1fs, ȷ=%d, nCμ=%.1f, st=%.1fs, lb=%.1f, Vμ=%.1e\n"::Cstring;t::Cdouble,ȷ::Cint,nCμ::Cdouble,st::Cdouble,lb::Cdouble,Vμ::Cdouble)::Cint)
+            ȷ = iA = 0
+        else
+            if iA === S
+                @ccall(printf("idle-M> t=%.1fs, lb=%.1f, Vμ=%.1e\n"::Cstring;t::Cdouble,lb::Cdouble,Vμ::Cdouble)::Cint)
+                iA = 0
+            else
+                iA += 1
+            end
         end
         s = take!(ch)
-        CanSpawn[s]=true
-        (; Ci, Cd) = sub[s]
+        (; Ci, Cd), CanSpawn[s] = sub[s], true
         ν, Cd[N+1] = Cd[N+1], -1.
-        ν>Vμ && (Gurobi.GRBaddconstr(o, N+1, Ci, Cd, Cchar('<'), -Cd[end], C_NULL); ȷ += 1)
+        ν>Vμ && (Gurobi.GRBaddconstr(o, N+1, Ci, Cd, Cchar('<'), -Cd[end], C_NULL); ȷ+=1)
         ν = ν>ϵ ? (α)ν+(1-α)ϵ : ϵ; VΣ=VΣ-first(Vv)+ν; Vμ=VΣ/S; push!(Vv,ν)
         while true
             brkFlag = CanSpawn[i]
-            brkFlag && (_5(ch, i, sub, mst, CanSpawn, Vμ); spnˈd += 1)
+            brkFlag && _5(ch, i, sub, mst, CanSpawn, Vμ)
             i = ifelse(i<S, i+1, 1)
             brkFlag && break
         end
     end
     for _=1:W_bg # wait for all threads to finish
         s = take!(ch)
-        (; Ci, Cd) = sub[s]
+        (; Ci, Cd), CanSpawn[s] = sub[s], true
         ν, Cd[N+1] = Cd[N+1], -1.
-        ν>Vμ && Gurobi.GRBaddconstr(o, N+1, Ci, Cd, Cchar('<'), -Cd[end], C_NULL)
+        ν>Vμ && (Gurobi.GRBaddconstr(o, N+1, Ci, Cd, Cchar('<'), -Cd[end], C_NULL); ȷ+=1)
         ν = ν>ϵ ? (α)ν+(1-α)ϵ : ϵ; VΣ=VΣ-first(Vv)+ν; Vμ=VΣ/S; push!(Vv,ν)
     end
 end
+function run_a_round(mst, sub; mstVType, addcut)
+    (; o, S, N, Θ, Bv, CanSpawn, ch, Xl) = mst
+    @assert S ≥ W_bg
+    if mstVType === 'B'
+        Gurobi.GRBsetintparam(Gurobi.GRBgetenv(o), "OutputFlag", 1)
+    elseif mstVType === 'C'
+        Gurobi.GRBsetintparam(Gurobi.GRBgetenv(o), "OutputFlag", 0)
+    else
+        error()
+    end
+    Bv .= mstVType; Gurobi.GRBsetcharattrarray(o, "VType", S, N, Bv)
+    Settings.opt_ass_opt(mst); load_che(mst)
+    if addcut
+        i = 1
+        for _=1:W_bg
+            _5(ch, i, sub, mst, CanSpawn, 0.)
+            i += 1
+        end
+        while i ≤ S
+            (; Ci, Cd) = sub[take!(ch)]
+            Cd[N+1] = -1; Gurobi.GRBaddconstr(o, N+1, Ci, Cd, Cchar('<'), -Cd[end], C_NULL)
+            _5(ch, i, sub, mst, CanSpawn, 0.)
+            i += 1
+        end
+        for _=1:W_bg
+            (; Ci, Cd) = sub[take!(ch)]
+            Cd[N+1] = -1; Gurobi.GRBaddconstr(o, N+1, Ci, Cd, Cchar('<'), -Cd[end], C_NULL)
+        end
+        Θ .= 1/S; Gurobi.GRBsetdblattrarray(o, "Obj", 0, S, Θ)
+        Settings.opt_ass_opt(mst); load_che(mst)
+    end
+    mstVType === 'B' && @. Xl = round(Xl)
+    lb = Settings.getmodeldblattr(mst, "ObjBound")
+    printstyled("mst's lb = $lb, xHa = $(repr(hash(Xl)))\n"; color = 30)
+    Gurobi.GRBsetintparam(Gurobi.GRBgetenv(o), "OutputFlag", 0)
+    lb
+end
+
+function leaf_train(mst, sub)
+    Models.retrofit!(mst)
+    h_old = UInt64(0)
+    for k = 1:typemax(Int)
+        cut_added, h = add_a_single_θ_cut(mst, sub; mstVType='B')
+        cut_added && continue
+        h_old === h && break
+        h_old = h
+    end
+end
+
+
 function load_che(mst)
     (; o, S, N, Θ, Xl) = mst
     Gurobi.GRBgetdblattrarray(o, "X", 0, S,  Θ)
